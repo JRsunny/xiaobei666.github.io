f26ed35b3a2556fad28a91870c6fd5639f1e9214
小北 <kubai666@126.com>
2019-11-25 11:29:43 +0800

    拍照测试1

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ function gotDevices(mediaDevices) {
   });
 }
 
-button.addEventListener('click', event => {
+// button.addEventListener('click', event => {
+  document.getElementById("button").addEventListener('click', event => {
   if (typeof currentStream !== 'undefined') {
     stopMediaTracks(currentStream);
   }
@@ -35,7 +36,7 @@ button.addEventListener('click', event => {
     alert(videoConstraints.facingMode)
   } else {
     videoConstraints.deviceId = { exact: select.value };
-    alert(JSON.stringify(videoConstraints))
+    alert(JSON.parse(videoConstraints))
   }
   const constraints = {
     video: videoConstraints,
@@ -52,6 +53,19 @@ button.addEventListener('click', event => {
     .catch(error => {
       console.error(error);
     });
+});
+
+// 拍照按钮 canvas截屏
+document.getElementById("btn-paizhao").addEventListener("click", function () {
+  var video = document.getElementById('video');
+  canvas = document.getElementById('canvasCemara');
+  ctx = canvas.getContext('2d');
+  var _w = 300, _h = 600;
+  if (video.videoWidth > 0) _h = video.videoHeight / (video.videoWidth / _w);
+  canvas.setAttribute('width', _w);
+  canvas.setAttribute('height', _h);
+  ctx.fillRect(0, 0, _w, _h);
+  ctx.drawImage(video, 0, 0, _w, _h);
 });
 
 navigator.mediaDevices.enumerateDevices().then(gotDevices);
